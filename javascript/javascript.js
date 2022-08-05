0692052fca216579e1bd3b3f8695209ce39d6d5b
Mary Joh <mary.eunie@gmail.com>
2022-08-05 14:53:52 -0400

    Define computer choice function

---
+++ javascript/javascript.js
@@ -0,0 +1,21 @@
+console.log("Hello World!")
+
+function getRandomArbitrary
+function getComputerChoice() {
+    switch()
+        case 0:
+            console.log("Rock!");
+            break;
+        case 1:
+            console.log("Paper!");
+            break;
+        case 2:
+            console.log("Scissors!");
+            break;
+        default:
+            console.log("Something went wrong...");
+
+
+
+}
+console.log()
